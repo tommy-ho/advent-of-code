5f4503e13cc3c089e903afc1fc00cc36bb2020e3
tommy-ho <tommy.ho@live.com>
2022-11-10 18:32:08 -0500

    2015/08 pt 2

--- 2015/08/solution.py
+++ 2015/08/solution.py
@@ -11,7 +11,6 @@ ans = 0
 
 for line in lines:
     line = line.strip()
-    print(line)
     total = len(line)
     chars_in_memory = 0
 
@@ -19,24 +18,42 @@ for line in lines:
         matches = re.findall(r'\\\"', line)
         chars_in_memory += len(matches)
         line = re.sub(r'\\\"', '', line)
-        print(f'chars in mem {chars_in_memory}')
 
         matches = re.findall(r'\\\\', line)
         chars_in_memory += len(matches)
         line = re.sub(r'\\\\', '', line)
-        print(f'chars in mem {chars_in_memory}')
 
         matches = re.findall(r'\\x[0-9a-f]{2}', line)
         chars_in_memory += len(matches)
         line = re.sub(r'\\x[0-9a-f]{2}', '', line)
-        print(f'chars in mem {chars_in_memory}')
 
-    print(line)
     chars_in_memory += len(line) #remaining chars
     chars_in_memory -= 2    #2 for the leading and trailing quotes
-    print(f'total {total}')
-    print(f'chars in mem {chars_in_memory}')
 
     ans += total - chars_in_memory
-    print(ans)
-    print()
+
+print(ans)
+
+
+# part 2
+
+ans = 0
+
+for line in lines:
+    line = line.strip()
+    total = len(line)
+    encoded_total = 0
+
+    matches = re.findall(r'\"', line)
+    encoded_total += len(matches) * 2
+    line = re.sub(r'\"', '', line)
+
+    matches = re.findall(r'\\', line)
+    encoded_total += len(matches) * 2
+    line = re.sub(r'\\', '', line)
+
+    encoded_total += len(line) + 2 #remaining chars and surrounding quotes
+
+    ans += encoded_total - total
+
+print(ans)
