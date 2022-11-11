9385feda558a5fc050884b03a9038cc19100d860
tommy-ho <tommy.ho@live.com>
2022-11-11 15:30:33 -0500

    2015/10 pt 1 and 2

--- 2015/10/solution.py
+++ 2015/10/solution.py
@@ -1,28 +1,33 @@
+# same code works for pt 1 and pt 2 (albeit a bit slower)
+
+def look_and_say(input, iterations):
+    while (iterations > 0):
+        temp = ''
+        val = None
+        count = 0
+
+        for i in range(len(input)):
+            if val is None: # start
+                val = input[i]
+                count += 1
+            elif input[i] == val: # same letter as before
+                count += 1
+            else: # diff letter, append to temp the last sequence
+                temp += str(count) + val
+                val = input[i]
+                count = 1
+        
+        # add to temp the last sequence if any
+        temp += str(count) + val
+
+        iterations -= 1
+        input = temp
+
+    return len(input)
+
+# part 1
 input = '1321131112'
-iterations = 40
+print(look_and_say(input, 40))
 
-while (iterations > 0):
-    temp = ''
-    val = None
-    count = 0
-
-    for i in range(len(input)):
-        if val is None: # start
-            val = input[i]
-            count += 1
-        elif input[i] == val: # same letter as before
-            count += 1
-        else: # diff letter, append to temp the last sequence
-            temp += str(count) + val
-            val = input[i]
-            count = 1
-    
-    temp += str(count) + val
-    # print(f'count is {count}')
-    # print(f'val is {val}')
-    # print(temp)
-
-    iterations -= 1
-    input = temp
-
-print(len(input))
+# part 2
+print(look_and_say(input, 50))
