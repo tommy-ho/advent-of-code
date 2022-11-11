57ec51868a0d5fa40515245ecc3418ecb718153c
tommy-ho <tommy.ho@live.com>
2022-11-11 00:54:25 -0500

    permutation solution for 2015/09 pt 1 and 2

--- 2015/09/solution.py
+++ 2015/09/solution.py
@@ -5,7 +5,8 @@ f = open('input.txt', 'r')
 flights = f.readlines()
 f.close()
 
-# part 1
+# part 1 - this approach does not always work if there are shortest/longest path 
+# that does not immediately take the most optimal option
 
 cost_table = {}
 cities = set()
@@ -56,3 +57,57 @@ for start in cities:
 
 shortest = min(matrix.items(), key=lambda x: x[1])
 print(shortest)
+
+
+
+# slightly less ideal path might lead to a better outcome, see below
+# dist = 804
+# Faerun selected longest flight to Norrath with dist 129
+# Norrath selected longest flight to Tristram with dist 142
+# Tristram selected longest flight to AlphaCentauri with dist 118       
+# AlphaCentauri selected longest flight to Arbre with dist 116
+# Arbre selected longest flight to Snowdin with dist 129
+# Snowdin selected longest flight to Straylight with dist 99
+# Straylight selected longest flight to Tambi with dist 70
+
+# dist < 804
+# Faerun selected longest flight to Norrath with dist 129
+# Norrath selected longest flight to Tristram with dist 142
+# Tristram selected longest flight to Arbre with dist 122
+# Arbre selected longest flight to Snowdin with dist 129
+# Snowdin selected longest flight to Straylight with dist 99
+# Straylight selected longest flight to AlphaCentauri with dist 91      
+# AlphaCentauri selected longest flight to Tambi with dist 18
+
+
+# part 2 - this approach solves both parts
+
+# capture all possible path permutations and then calculate them using cost table
+
+paths = []
+for city in cities:
+    paths.append([city])
+
+while True:
+    new_paths = []
+    for path in paths:
+        for city in cities:
+            if city not in path:
+                new_path = path + [city]
+                new_paths.append(new_path)
+    paths = new_paths
+    if len(paths[0]) == len(cities):
+        break
+
+highest = 0
+lowest = sys.maxsize
+
+for path in paths:
+    dist = 0
+    for i in range(0, len(path)-1):
+        dist += cost_table[(path[i], path[i+1])]
+    highest = max(dist, highest)
+    lowest = min(dist, lowest)
+
+print(lowest)
+print(highest)
