1901554ac769a254c6eda02fb8119da1dfa9c26b
tommy-ho <tommy.ho@live.com>
2022-11-14 23:39:34 -0500

    2015/12 pt 1

---
+++ 2015/12/solution.py
@@ -0,0 +1,28 @@
+import json
+
+# reading input
+f = open('input.txt', 'r')
+data = json.load(f)
+f.close()
+
+# part 1
+
+def search(data):
+    if type(data) is int:
+        return data
+    elif type(data) is str:
+        return 0
+    
+    sum = 0
+    for key in data:
+        if type(key) is int:
+            sum += key
+        elif type(key) in [list, dict]:
+            sum += search(key)    
+        elif type(key) is str and type(data) is dict: # not list
+            sum += search(data[key])
+
+    return sum
+
+sum = search(data)
+print(sum)
