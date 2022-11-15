43f9ca41b9422ca02196998c3f3073ff56ee1701
tommy-ho <tommy.ho@live.com>
2022-11-15 17:22:59 -0500

    2015/12 pt 2

--- 2015/12/solution.py
+++ 2015/12/solution.py
@@ -7,8 +7,32 @@ f.close()
 
 # part 1
 
+# def search(data):
+#     if type(data) is int:
+#         return data
+#     elif type(data) is str:
+#         return 0
+    
+#     sum = 0
+#     for key in data:
+#         if type(key) is int:
+#             sum += key
+#         elif type(key) in [list, dict]:
+#             sum += search(key)    
+#         elif type(key) is str and type(data) is dict: # not list
+#             sum += search(data[key])
+
+#     return sum
+
+# sum = search(data)
+# print(sum)
+
+# part 2
+
 def search(data):
-    if type(data) is int:
+    if type(data) is dict and 'red' in data.values():
+        return 0
+    elif type(data) is int:
         return data
     elif type(data) is str:
         return 0
