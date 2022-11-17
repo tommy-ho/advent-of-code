f0b53f7d79ac088e1c883e28e3c914cb1682b68c
tommy-ho <tommy.ho@live.com>
2022-11-17 18:54:14 -0500

    uncomment

--- 2015/12/solution.py
+++ 2015/12/solution.py
@@ -7,25 +7,25 @@ f.close()
 
 # part 1
 
-# def search(data):
-#     if type(data) is int:
-#         return data
-#     elif type(data) is str:
-#         return 0
+def search(data):
+    if type(data) is int:
+        return data
+    elif type(data) is str:
+        return 0
     
-#     sum = 0
-#     for key in data:
-#         if type(key) is int:
-#             sum += key
-#         elif type(key) in [list, dict]:
-#             sum += search(key)    
-#         elif type(key) is str and type(data) is dict: # not list
-#             sum += search(data[key])
+    sum = 0
+    for key in data:
+        if type(key) is int:
+            sum += key
+        elif type(key) in [list, dict]:
+            sum += search(key)    
+        elif type(key) is str and type(data) is dict: # not list
+            sum += search(data[key])
 
-#     return sum
+    return sum
 
-# sum = search(data)
-# print(sum)
+sum = search(data)
+print(sum)
 
 # part 2
 
