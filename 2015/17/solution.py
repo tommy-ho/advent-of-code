3d58bc69b821ef4b7ff2c3528a71d8f93d879d9f
Ho, Tommy <tommy.ho@live.com>
2022-11-23 03:06:45 -0500

    2015/17 pt 2

--- 2015/17/solution.py
+++ 2015/17/solution.py
@@ -1,22 +1,24 @@
+from itertools import combinations
+import sys
+
 # reading input
 f = open('input.txt', 'r')
 lines = f.readlines()
 containers = [int(i) for i in lines]
 f.close()
 
-# part 1
+# part 1 - DP
 
 containers.sort()
 containers.insert(0, 0)
-
 liters = 150
 
+# define dp table and base cases
 dp = []
 for i in range(len(containers)):
     dp.append([])
     for j in range(liters+1):
         dp[i].append(0)
-
 
 
 for i in range(1, len(containers)):
@@ -28,9 +30,26 @@ for i in range(1, len(containers)):
             dp[i][j] += dp[i-1][j-container]
         dp[i][j] += dp[i-1][j]
 
-i = 0
-for row in dp:
-    print(containers[i])
-    print(row)
-    print()
-    i += 1
+
+print(dp[-1][-1])
+
+
+# part 2 - brute force all combinations
+
+combos = []
+for i in range(1, len(containers)):
+    for combo in combinations(containers, i):
+        combos.append(combo)
+
+min_containers_used = sys.maxsize
+ans = 0
+
+for combo in combos:
+    if sum(combo) == liters:
+        if len(combo) < min_containers_used:
+            ans = 1
+            min_containers_used = len(combo)
+        elif len(combo) == min_containers_used:
+            ans += 1
+
+print(ans)
