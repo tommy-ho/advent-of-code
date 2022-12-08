a7bcdbc56d8f82e107841046a1c6f76f208a8333
tommy-ho <tommy.ho@live.com>
2022-12-07 23:12:21 -0500

    2022/07 pt 2

--- 2022/07/solution.py
+++ 2022/07/solution.py
@@ -4,6 +4,8 @@ input = f.readlines()
 input = [i.strip() for i in input]
 f.close()
 
+# part 1
+
 class Item:
     def __init__(self, name, type, size=0, parent=None):
         self.name = name
@@ -20,9 +22,6 @@ current = None
 root = None
 
 for line in input:
-    # if current is not None:
-    #     print(f'currently in {current.name}')
-    # print(line)
     line = line.split(' ')
     if line[0] == '$':
         cmd = line[1]
@@ -32,13 +31,12 @@ for line in input:
                 root = current
             elif line[2] == '..':
                 current = current.parent
-                # print(f'moving up dir to {current.name}')
             elif line[2] == '/':
                 current = root
             else:
                 current = current.children[line[2]]
         # elif cmd == 'ls':
-            # continue -- do nothing because it will get handled at next iteration
+            #  do nothing because it will get handled at next iteration
     elif line[0] == 'dir': # add dir as a child to current
         current.children[line[1]] = Item(current.name + '/' + line[1], 'dir', parent=current)
     else: # it's a file
@@ -54,18 +52,12 @@ q.append(root)
 while (len(q) > 0):
     item = q.pop(0)
 
-    # try:
-    #     print(f'{item.name} of size {item.size} with parent {item.parent.name}')
-    # except:
-    #     print()
-
     if item.type == 'dir':
         dir_sizes[item.name] = item.size
 
     parent = item.parent
 
     while parent is not None:
-        # print(f'updating {parent.name} by adding {item.size}')
         dir_sizes[parent.name] += item.size
         parent = parent.parent
 
@@ -73,12 +65,39 @@ while (len(q) > 0):
         c = item.children[child]
         q.append(c)
     
-
-# print(dir_sizes)
-
 ans = 0
 for dir in dir_sizes:
     if dir_sizes[dir] <= 100000:
         ans += dir_sizes[dir]
 
 print(ans)
+
+# part 2
+
+used = 0
+
+q = []
+q.append(root)
+while (len(q) > 0):
+    item = q.pop(0)
+
+    if item.type == 'file':
+        used += item.size
+
+    parent = item.parent
+
+    for child in item.children:
+        c = item.children[child]
+        q.append(c)
+
+total = 70000000
+required = 30000000
+need = required - (total - used)
+smallest = None
+
+for dir in dir_sizes:
+    if dir_sizes[dir] >= need:
+        if smallest is None or dir_sizes[dir] < smallest[1]:
+            smallest = (dir, dir_sizes[dir])
+
+print(smallest)
