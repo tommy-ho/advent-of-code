9bc15886ed82e7ba91cbd3539409c3376fd847db
tommy-ho <tommy.ho@live.com>
2022-12-17 01:01:09 -0500

    2022/10 part 2

--- 2022/10/solution.py
+++ 2022/10/solution.py
@@ -2,6 +2,7 @@
 f = open('input.txt', 'r')
 input = f.readlines()
 instructions = [i.strip() for i in input]
+instructions2 = instructions.copy()
 f.close()
 
 # part 1
@@ -21,7 +22,7 @@ for i in range(0, cycles):
     if is_cycle(i+1):
         signal_strengths.append(X * (i+1))
 
-    print(f'cycle {i+1} : {X * (i+1)}')
+    # print(f'cycle {i+1} : {X * (i+1)}')
 
     if wait:
         X += to_add
@@ -38,3 +39,45 @@ for i in range(0, cycles):
     
 
 print(sum(signal_strengths))
+
+# part 2
+
+cycles = 240
+pixels = []
+X = 1 # register starts at 1
+wait = False
+to_add = None
+
+for i in range(0, cycles):
+    position = i % 40
+    if position in [X-1, X, X+1]:
+        pixels.append('#')
+    else:
+        pixels.append('.')
+
+    # print(f'cycle {i+1} : {X}')
+
+    if wait:
+        X += to_add
+        wait = False
+    else:
+        # cycle through the instructions
+        instr = instructions2.pop(0)
+        instructions2.append(instr)
+
+        if instr != 'noop':
+            split = instr.split(' ')
+            to_add = int(split[1])
+            wait = True
+    
+
+row_limit = 40
+current_row = ''
+for i in range(len(pixels)):
+    if row_limit > 0:
+        current_row += pixels[i]
+        row_limit -= 1
+    if row_limit == 0:
+        print(current_row)
+        current_row = ''
+        row_limit = 40
