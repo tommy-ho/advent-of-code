d1c0b35feb9e2931d5d62cbd1e98f35ff9a1b26a
tommy-ho <tommy.ho@live.com>
2022-12-29 16:30:14 -0500

    2022/15 part 2 too slow doesn't work

--- 2022/15/solution.py
+++ 2022/15/solution.py
@@ -21,7 +21,7 @@ for line in lines:
 
     dist = abs(sx-bx) + abs(sy-by)
 
-    if dist < abs(sy-row): # skip if row is too far from beacon
+    if dist < abs(sy-row): # skip if row is too far from sensor
         continue
 
     dist -= abs(sy-row) # decrease dist by distance from row
@@ -35,3 +35,40 @@ for beacon in beacons:
         del grid[beacon]
 
 print(len(grid))
+
+
+# part 2
+
+max = 4000000
+grid = {}
+beacons = []
+
+x_counter = 0
+y_counter = 0
+
+for line in lines:
+    split = line.split('=')
+    sx = int(split[1].split(',')[0])
+    sy = int(split[2].split(':')[0])
+    bx = int(split[3].split(',')[0])
+    by = int(split[4].strip())
+
+    beacons.append((bx,by)) # add a list of beacons to remove from list later
+
+    dist = abs(sx-bx) + abs(sy-by)
+
+    for row in range(max):            
+        if dist < abs(sy-row): # skip if row is too far from sensor
+            continue
+
+        start = sx - (dist - abs(sy-row))
+        end = sx + (dist - abs(sy-row))
+        for i in range(start, end + 1):
+            grid[i, row] = '#'
+
+
+for x in range(max):
+    for y in range(max):
+        if (x,y) not in grid:
+            print(f'{x}  {y}')
+            print(x*4000000 + y)
